50bcc23340789ae5a34d8a2c05d55e5fabc23e3f
Iñigo Asensio <inigo.asensio@sage.com>
2016-02-13 14:02:10 +0100

    Update Config

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ OnlineStore::Application.routes.draw do
 
   resources :products, :orders, :payments
 
+  get "carts/show"
+
+  resource :cart, only: [:show] do
+    #put 'add/:product_id', to: 'carts#add', as: :add_to
+    put 'remove/:product_id', to: 'carts#remove', as: :remove_from
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@
 Product.delete_all
 
 products = [
-  { name: 'Computer', unit_price: 599.99, description: 'computer', unit_quantity: 1, image: File.open("#{Rails.root}/app/assets/images/default_product.png") },
-  { name: 'Tablet', unit_price: 99.99, description: 'tablet', unit_quantity: 7, image: File.open("#{Rails.root}/app/assets/images/default_product.png") },
-  { name: 'Smartphone', unit_price: 399.99, description: 'smartphone', unit_quantity: 4, image: File.open("#{Rails.root}/app/assets/images/default_product.png") },
-  { name: 'Camera', unit_price: 49.99, description: 'camera', unit_quantity: 2, image: File.open("#{Rails.root}/app/assets/images/default_product.png") }
+  { id: 1, name: 'Computer', unit_price: 599.99, description: 'computer', unit_quantity: 1, image: File.open("#{Rails.root}/app/assets/images/default_product.png") },
+  { id: 2, name: 'Tablet', unit_price: 99.99, description: 'tablet', unit_quantity: 7, image: File.open("#{Rails.root}/app/assets/images/default_product.png") },
+  { id: 3, name: 'Smartphone', unit_price: 399.99, description: 'smartphone', unit_quantity: 4, image: File.open("#{Rails.root}/app/assets/images/default_product.png") },
+  { id: 4, name: 'Camera', unit_price: 49.99, description: 'camera', unit_quantity: 2, image: File.open("#{Rails.root}/app/assets/images/default_product.png") }
 ]
 
 products.each do |product|
