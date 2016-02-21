3fc1d9bb74fd6d260925a6d4db25811c4da16c48
Iñigo Asensio <inigo.asensio@sage.com>
2016-02-21 22:52:05 +0100

    Update Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate_user!
   helper_method :current_order
 
   def current_order
-    if session[:order_id].nil?
-      current_user.orders.build
-    else
-      current_user.orders.find(session[:order_id])
+    if user_signed_in?
+      if session[:order_id].nil?
+        orders = Order.new
+        current_user.orders.build
+      else
+        current_user.orders.find(session[:order_id])
+      end
     end
   end
 
