586f77a43d721a5fbdf0c1c03b92b794102ec7e0
Iñigo Asensio <inigo.asensio@sage.com>
2016-05-02 21:11:09 -0400

    Fix Addreses

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ActiveRecord::Base
   # Associations
   belongs_to :addressable, polymorphic: true
-  self.inheritance_column = :type
+  # self.inheritance_column = :type
 
   # Attributes
   attr_accessible :address1, :type, :city, :country, :postal_code, :state
@@ -9,7 +9,7 @@ class Address < ActiveRecord::Base
   # Validations
   validates :address1, :type, :city, :country, :postal_code, :state, presence: true
 
-  def self.type
-    %w(shipping_address billing_address)
-  end
+  # def self.type
+  #   %w(shipping_address billing_address)
+  # end
 end
